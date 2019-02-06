1f75cb1cdbf48f37fbf89eef11c8d3be43132037
Javontay McElroy <dinomightnetwork@gmail.com>
2019-02-06 16:31:46 -0600

    Create Account page styled!

--- artfolio2.0/src/components/auth/CreateAccount.js
+++ artfolio2.0/src/components/auth/CreateAccount.js
@@ -26,31 +26,34 @@ class CreateAccount extends Component {
 
     render() {
         return (
-            <div className="row container card" style={{margin:"40px auto", padding:"60px"}}>
+            <div className="create-account-card">
                 <form onSubmit={this.createAccount} className="col s12">
                     <div className="row">
+                        <label className="create-title">First Name</label>
                         <div className="input-field col s6">
-                        <input onChange={this.handleChanges} value={this.state.firstName} name="firstName" type="text" className="validate"/>
-                        <label>First Name</label>
+                        <input onChange={this.handleChanges} value={this.state.firstName} name="firstName" type="text" className="validate-create"/>
                         </div>
+
+                        <label className="create-title">Last Name</label>
                         <div className="input-field col s6">
-                        <input onChange={this.handleChanges} value={this.state.lastName} name="lastName" type="text" className="validate"/>
-                        <label>Last Name</label>
+                        <input onChange={this.handleChanges} value={this.state.lastName} name="lastName" type="text" className="validate-create"/>
                         </div>
                     </div>
+
                     <div className="row">
+                        <label className="create-title">Email</label>
                         <div className="input-field col s12">
-                        <input onChange={this.handleChanges} value={this.state.email} name="email" type="email" className="validate"/>
-                        <label>Email</label>
+                        <input onChange={this.handleChanges} value={this.state.email} name="email" type="email" className="validate-create"/>
                         </div>
                     </div>
+
                     <div className="row">
+                        <label className="create-title">Password</label>
                         <div className="input-field col s12">
-                        <input onChange={this.handleChanges} value={this.state.password} name="password" type="password" className="validate"/>
-                        <label>Password</label>
+                        <input onChange={this.handleChanges} value={this.state.password} name="password" type="password" className="validate-create"/>
                         </div>
                     </div>
-                    <button class="waves-effect waves-light btn #00695c teal darken-3">Create Account</button>
+                    <button class="create-account-btn">Create Account</button>
                 </form>
         </div>
         )
