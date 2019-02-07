12fd6188bf12b969471476752996e0651764309f
Angel Torres <alas4224@gmail.com>
2019-02-06 19:11:19 -0600

    made a few changes

--- artfolio2.0/src/components/auth/SignIn.js
+++ artfolio2.0/src/components/auth/SignIn.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { signIn } from '../../actions'
+import { signIn } from '../../actions';
+
 
 class SignIn extends Component {
 
